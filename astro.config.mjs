626658b588aee2a8fb68633b090d9f7afb4c7b72
avolent <will@slamped.com>
2022-12-04 17:57:45 +1100

    Add Mdx support and update index page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
+import mdx from "@astrojs/mdx";
+
 export default defineConfig({
-    site: 'https://avolent.io',
-    base: '/cortex',
+  site: 'https://avolent.io',
+  base: '/cortex',
+  integrations: [mdx()]
 });
