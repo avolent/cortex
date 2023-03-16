e556e244a8035b0f1a0cfc90a7b4c795100c9309
Will <40684390+avolent@users.noreply.github.com>
2023-03-16 17:28:29 +1100

    Revert changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,5 @@ import mdx from "@astrojs/mdx";
 export default defineConfig({
     site: 'https://avolent.io',
     base: '/cortex',
-    integrations: [mdx()],
-    metadata: {
-        lastUpdated: new Date().toISOString()
-    }
+    integrations: [mdx()]
 });
