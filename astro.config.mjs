fdb718940e79283b7907bbf63e936101def226e6
Will <40684390+avolent@users.noreply.github.com>
2023-03-16 16:13:10 +1100

    Add last updated support

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,7 @@ export default defineConfig({
     site: 'https://avolent.io',
     base: '/cortex',
     integrations: [mdx()]
+    metadata: {
+        lastUpdated: new Date().toISOString()
+    }
 });
